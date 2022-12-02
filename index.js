0bfead3ffdbcbb0d8a862dffc78c49a228fbabcb
JooJo <sjak0101@gmail.com>
2022-12-02 10:43:33 +0000

    added get method for loading data to drawar dashboard

--- index.js
+++ index.js
@@ -26,25 +26,31 @@ async function run(){
         res.send(services);
        });
 
+       // This is not the proper way to query. 
+       // After learning more about mongodb. use aggregate lookup, pipeline, match, group 
 
        app.get('/available', async(req, res) =>{
-        const date = req.query.date || 'May 11, 2022';
+        const date = req.query.date;
 
         // step : 1 get all services
 
         const services = await servicesCollection.find().toArray();
 
-        // step 2: get the booking of that day
+        // step 2: get the booking of that day. output [{}, {}, {}, {}]
         const query = {date: date};
         const bookings = await bookingCollection.find(query).toArray();
 
         // step 3: for each service, find bookings for that service
-        services.forEach(service =>{
-          const serviceBooking = bookings.filter(b => b.treatment === service.name);
-          const booked = serviceBooking.map(s=> s.slot);
-          const available = service.slots.filter(s=>!booked.includes(s));
-          service.available = available;
-        })
+        services.forEach(service=>{
+          // step 4: find bookings for that service. output:[{},{},{},{}]
+          const serviceBookings = bookings.filter(book => book.treatment === service.name);
+          // step 5: select slots for service Bookings: ['','','','',]
+          const bookedSlots = serviceBookings.map(book => book.slot);
+          // step 6: select those slots that are not in bookedSlots
+          const available = service.slots.filter(slot => !bookedSlots.includes(slot));
+          // step 7: set availabe to slots to make it easier
+          service.slots = available;
+        });
 
 
         res.send(services);
@@ -59,6 +65,13 @@ async function run(){
        * app.patch('/booking/:id') // 
        * app.delete('/booking/:id') // 
        */
+
+       app.get('/booking', async(req, res) =>{
+        const patient = req.query.patient;
+        const query = {patient: patient};
+        const bookings = await bookingCollection.find(query).toArray();
+        res.send(bookings);
+       })
 
        app.post('/booking', async(req, res) =>{
             const booking = req.body;
